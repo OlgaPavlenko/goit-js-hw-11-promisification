3a313f18ecae9fa1a2aa6800f851132297161807
Olga Pavlenko <olgaigpavlenko@gmail.com>
2020-11-13 18:23:40 +0200

    renamed logger

--- delay.js
+++ delay.js
@@ -7,5 +7,5 @@ const loggerRes = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
 delay(2000).then(loggerRes); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(1000).then(loggerRes); // Resolved after 1000ms
+delay(1500).then(loggerRes); // Resolved after 1500ms
